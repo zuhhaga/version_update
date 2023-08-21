5f56398948d69c3c88aaab43078dfb429905f5b9
zuhhaga <141036972+zuhhaga@users.noreply.github.com>
2023-08-21 19:49:16 +0300

    Update waydroid_update.pl

--- waydroid_update.pl
+++ waydroid_update.pl
@@ -1,5 +1,6 @@
 use JSON::PP;
 use LWP::Simple;
+use experimental "try";
 
 my @json = (
 '/waydroid/lineage-vendor-mainline.arm64.zip'  , 'https://ota.waydro.id/vendor/waydroid_arm64/MAINLINE.json',
@@ -15,22 +16,28 @@ my @json = (
 open(my $fh, '>>', 'docs/_redirects');
 
 my $ts = 0;
-
+my $id = 1;
+try {
 while ($#json) {
     my $resp = get(pop @json);
     my $json = decode_json($resp);
-    my $temp = $json->{'response'}[0]{'datetime'};
+    my $temp = $json->{'response'}[0]{'datetime'} + 0;
+    
     if ($temp > $ts){
         $ts = $temp;
     }
     my $url = $json->{'response'}[0]{'url'};
     print $fh (pop @json, ' ', $url, "\n");
+    $id = $id + 1;
+}
+} catch ($err){
+    
 }
 
 close $fh;
+my $version = POSIX::strftime("%m%d%Y", localtime($ts));
 
-my $version = POSIX::strftime("%m%d%Y", $ts);
-
-open(my $fh, '>', 'docs/waydroid_version');
+open(my $fh, '>', 'waydroid_version');
 print $fh ($version);
 close $fh;
+
